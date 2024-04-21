5109b3592ef0b2bdb48b70f9000a940040d9b051
hlibk <hlibk@proton.me>
2024-04-20 19:34:06 -0600

    Finish test case graph

--- act_2.1_perceptron_adaline.py
+++ act_2.1_perceptron_adaline.py
@@ -1,7 +1,7 @@
 import random
 import matplotlib.pyplot as plt
 
-# populate example database with random data
+# populate example database with random data for case 1
 class case1_data_generation:
     filename = ""
 
@@ -29,6 +29,7 @@ class case1_data_generation:
             
             i += 1
 
+# create logistic map and the x values for case 2
 class case2_data_generation:
     filename = ""
 
@@ -47,7 +48,7 @@ class case2_data_generation:
             i += 1
         i = 0
         with open("logistic_map.csv", "a") as f:
-            while i < 1000:
+            while i < 1103:
                 x = self.logistic_formula(x)
                 f.write(str(x) + "\n")
                 i += 1
@@ -61,11 +62,12 @@ class case2_data_generation:
                     v.write("{},{},{},{}\n".format(logistics[i][:-1], logistics[i+1][:-1], logistics[i+2][:-1], logistics[i+3][:-1]))
                     i += 1
 
-
 class Perceptron:
     weight = []
     rate = 0
     epochs = 0
+
+    error = []
 
     def __init__(self, rate, epochs):
         self.rate = rate
@@ -82,6 +84,7 @@ class Perceptron:
         return 1 if activation >= 0.0 else 0
 
     def fit(self, dataset):
+        self.error = []
         self.weight = [0.0 for i in range(len(dataset[0]))]
 
         for epoch in range(self.epochs):
@@ -98,12 +101,24 @@ class Perceptron:
                     self.weight[i + 1] += self.rate * error * row[i]
                     i += 1
                 
-            print("Epoch {}: Error Sum = {}".format(epoch+1, error_sum))
+            self.error.append(error_sum)
+            
+        self.plot_error()
+
+    def plot_error(self):
+        plt.plot(self.error)
+        plt.grid()
+        plt.xlabel("Epochs")
+        plt.ylabel("Error Amount")
+        plt.show()
+                
 
 class Adaline:
     weight = []
     rate = 0
     epochs = 0
+
+    error = []
 
     def __init__(self, rate, epochs):
         self.rate = rate
@@ -123,6 +138,7 @@ class Adaline:
         return 1 if self.activate(row) >= 0.5 else 0
 
     def fit(self, dataset):
+        self.error = []
         self.weight = [0.0 for i in range(len(dataset[0]))]
 
         for epoch in range(self.epochs):
@@ -139,9 +155,18 @@ class Adaline:
                     self.weight[i + 1] += self.rate * error * row[i]
                     i += 1
                 
-            print("Epoch {}: Error Sum = {}".format(epoch+1, error_sum))
+            self.error.append(error_sum)
+            
+        self.plot_error()
 
-class TestDataPrediction:
+    def plot_error(self):
+        plt.plot(self.error)
+        plt.grid()
+        plt.xlabel("Epochs")
+        plt.ylabel("Error Amount")
+        plt.show()
+
+class case1_prediction:
     file = ""
 
     def __init__(self, file):
@@ -151,32 +176,62 @@ class TestDataPrediction:
         with open(self.file, "r") as f:
             rows = f.readlines()
 
-            accurate_total = 0
-            inaccurate_total = 0
+            true_positive = 0
+            false_positive = 0
+            true_negative = 0
+            false_negative = 0
 
-            k = 3
-            curk = 0
-            row = 0
-
-            while curk < k:
-                accurate = 0
-                inaccurate = 0
-
-                while row < (len(rows)/3)*(curk+1):
-                    prepared_row = [int(x) for x in rows[row][:-1].split(",")]
-                    predicted = model.predict(prepared_row)
-                    # print("{}. Predicted: {}. Expected: {}. Error: {}".format(prepared_row, predicted, prepared_row[-1], predicted != prepared_row[-1]))
-                    if predicted == prepared_row[-1]:
-                        accurate += 1
+            for row in rows:
+                prepared_row = [int(x) for x in row[:-1].split(",")]
+                predicted = model.predict(prepared_row)
+                # print("{}. Predicted: {}. Expected: {}. Error: {}".format(prepared_row, predicted, prepared_row[-1], predicted != prepared_row[-1]))
+                if predicted == prepared_row[-1]:
+                    if predicted == 1:
+                        true_positive += 1
                     else:
-                        inaccurate += 1
-                    row += 1
-                print("Accuracy rating with test data k={}: {:.2f}%".format(curk+1, accurate/(accurate+inaccurate)*100))
-                accurate_total += accurate
-                inaccurate_total += inaccurate
-                curk += 1
-            print("\nMean accuracy rating: {:.2f}%".format(accurate_total/(accurate_total+inaccurate_total)*100))
+                        true_negative += 1
+                else:
+                    if predicted == 1:
+                        false_positive += 1
+                    else:
+                        false_negative += 1
+            precision = (true_positive / (true_positive + false_positive))*100
+            recall = (true_positive / (true_positive + false_negative))*100
+            f_measure = ((2 * true_positive) / (2 * true_positive + false_positive + false_negative))*100
+            accuracy = ((true_positive + true_negative) / (true_positive + true_negative + false_positive + false_negative))*100
+            print("\nPrecision: {:.2f}%, Recall: {:.2f}%, F measure: {:.2f}%".format(precision, recall, f_measure))
+            print("Mean accuracy rating: {:.2f}%".format(accuracy))
 
+class case2_prediction:
+    file = ""
+    y = []
+    predicted_y = []
+    error = []
+
+    def __init__(self, file):
+        self.file = file
+
+    def predict(self, model):
+        self.y = []
+        self.predicted_y = []
+        self.error = []
+
+        with open(self.file, "r") as f:
+            for row in f.readlines()[1000:]:
+                prepared_row = [float(x) for x in row.split(",")]
+                prepared_row = [prepared_row[0], prepared_row[1], prepared_row[2], prepared_row[0]**2, prepared_row[1]**2, prepared_row[2]**2, prepared_row[3]]
+                self.y.append(prepared_row[-1])
+                self.predicted_y.append(model.activate(prepared_row))
+                self.error.append((self.y[-1] - self.predicted_y[-1])**2)
+        
+        self.plot_prediction()
+    
+    def plot_prediction(self):
+        plt.plot(self.y, self.y, label="Expected")
+        plt.plot(self.predicted_y, self.y, label="Predicted")
+        plt.grid()
+        plt.legend(loc="best")
+        plt.show()
 
 dataset = [[0,0,0,0,0],
 [0,0,0,1,0],
@@ -197,54 +252,41 @@ dataset = [[0,0,0,0,0],
 
 filename = "chaos_values.csv"
 
-# CASE 1 PREPARATION
-# prepare_test_data = case1_data_generation(filename)
-# prepare_test_data.populate(1000)
+def case1_perceptron():
+    filename = "trainingdata.csv"
+    perceptron = Perceptron(0.01, 100)
+    perceptron.fit(dataset)
 
-# CASE 1 FITTING
-# perceptron = Perceptron(0.01, 100)
-# perceptron.fit(dataset)
+    test = case1_prediction(filename)
+    test.predict(perceptron)
 
-# print()
+def case1_adaline():
+    filename = "trainingdata.csv"
+    adaline = Adaline(0.01, 100)
+    adaline.fit(dataset)
 
-# adaline = Adaline(0.1, 100)
-# adaline.fit(dataset)
+    test = case1_prediction(filename)
+    test.predict(adaline)
 
-# CASE 1 TESTING
-# test = TestDataPrediction(filename)
-# print("\nPerceptron")
-# test.predict(perceptron)
-# print("\nAdaline")
-# test.predict(adaline)
+def case2_adaline():
+    filename = "chaos_values.csv"
+    adaline = Adaline(0.001, 1000)
 
-# CASE 2 PREPARATION
-# logic_map = case2_data_generation("chaos_values.csv")
-# logic_map.logistic_map()
-# logic_map.logistic_values()
+    prepared_dataset = []
 
-# CASE 2 FITTING
-prepared_dataset = []
-with open(filename, "r") as f:
-    prepared_dataset = [list(map(float, x[:-1].split(","))) for x in f.readlines()]
+    with open(filename, "r") as f:
+        prepared_dataset = [list(map(float, x[:-1].split(","))) for x in f.readlines()[:999]]
+    
+    prepared_dataset_redundancy = []
+    for row in prepared_dataset:
+        row_redundancy = [row[0], row[1], row[2], row[0]**2, row[1]**2, row[2]**2, row[3],]
+        prepared_dataset_redundancy.append(row_redundancy)
 
-prepared_dataset_redundancy = []
-for row in prepared_dataset:
-    row_redundancy = [row[0], row[1], row[2], row[0]**2, row[1]**2, row[2]**2, row[3],]
-    prepared_dataset_redundancy.append(row_redundancy)
+    adaline.fit(prepared_dataset_redundancy)
 
-adaline = Adaline(0.001, 1000)
-adaline.fit(prepared_dataset_redundancy)
+    test = case2_prediction(filename)
+    test.predict(adaline)
 
-y = []
-predicted_y = []
-
-for row in prepared_dataset_redundancy:
-    y.append(row[-1])
-    predicted_y.append(adaline.activate(row))
-
-print(y)
-print(predicted_y)
-
-plt.plot(y, y)
-plt.plot(predicted_y, y)
-plt.show()
+# case1_perceptron()
+# case1_adaline()
+case2_adaline()
